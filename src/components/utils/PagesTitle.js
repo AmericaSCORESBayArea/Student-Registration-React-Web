a883df216a9985572e7dfd842e2340cdcc3b2e68
lautirozen <lautirozen@gmail.com>
2022-08-26 19:32:04 -0300

    added page title

---
+++ src/components/utils/PagesTitle.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PagesTitle() {
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <h1>Student Registration</h1>
+    </div>
+  );
+}
+
+export { PagesTitle };
