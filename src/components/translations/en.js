dd90b2108254d0773a2311c2a496529eb7ba2cd0
lautirozen <lautirozen@gmail.com>
2022-08-26 19:31:14 -0300

    added english traslations

---
+++ src/components/translations/en.js
@@ -0,0 +1,25 @@
+export const enLanguages = {
+  US: "English",
+  ES: "Spanish",
+  CN: "Chinese (simplified)",
+  login_warning:
+    "You need to be logged in to access the student registration form.",
+  error_404: "Error 404. Page not found.",
+  parent_coach_title:
+    "Please select whether you are a Parent/Guardian of a scores student or a Coach.",
+  parent_coach_option:
+    "Are you submitting this form as a Parent/Guardian or a Coach?",
+  parent_option: "Parent/Guardian",
+  coach_option: "Coach",
+  new_option: "New",
+  returning_option: "Returning",
+  new_returning_option: "New or returning?",
+  new_returning_title:
+    "Select whether you are starting a new student registration or updating a returning student registration.",
+  steps_1: "Select your role type",
+  steps_2: "Select new or returning",
+  steps_3: "Complete form",
+  button_continue: "CONTINUE",
+  button_back: "BACK",
+  button_submit: "SUBMIT",
+};
