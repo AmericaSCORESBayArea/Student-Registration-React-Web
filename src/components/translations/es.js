f153686aea3d318e0625303e124d9f2df42abb8d
lautirozen <lautirozen@gmail.com>
2022-08-26 19:31:37 -0300

    added spanish traslations

---
+++ src/components/translations/es.js
@@ -0,0 +1,25 @@
+export const esLanguages = {
+  US: "Inglés",
+  ES: "Español",
+  CN: "Chino (simplificado)",
+  login_warning:
+    "Es necesario iniciar sesión para acceder al formulario de registro de estudiantes.",
+  error_404: "Error 404. Página no encontrada",
+  parent_coach_title:
+    "Seleccione si es un padre/tutor de un estudiante de scores o un entrenador.",
+  parent_coach_option:
+    "¿Está enviando este formulario como padre/tutor o entrenador?",
+  parent_option: "Padre/Tutor",
+  coach_option: "Entrenador",
+  new_option: "Nuevo",
+  returning_option: "Existente",
+  new_returning_option: "¿Nuevo o existente?",
+  new_returning_title:
+    "Seleccione si está iniciando un nuevo registro de estudiante o actualizando el registro de un estudiante existente.",
+  steps_1: "Seleccione su rol",
+  steps_2: "Seleccione nuevo o existente",
+  steps_3: "Complete el formulario",
+  button_continue: "CONTINUAR",
+  button_back: "VOLVER",
+  button_submit: "ENVIAR",
+};
