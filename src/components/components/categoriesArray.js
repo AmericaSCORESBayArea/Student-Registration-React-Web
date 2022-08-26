1ade520a5b0775ecece69d36b77dc3894832bf97
lautirozen <lautirozen@gmail.com>
2022-08-26 19:29:03 -0300

    added categoriesArray

---
+++ src/components/components/categoriesArray.js
@@ -0,0 +1,20 @@
+export const categoriesArray = [
+  {
+    id: 0,
+    name: "Parent",
+  },
+  {
+    id: 1,
+    name: "Coach",
+  },
+];
+export const statusArray = [
+  {
+    id: 0,
+    name: "New",
+  },
+  {
+    id: 1,
+    name: "Existing",
+  },
+];
