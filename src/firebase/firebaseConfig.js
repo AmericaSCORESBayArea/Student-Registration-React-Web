29b7438ee5fa445345669295ae512cdf7d43fb55
lautirozen <lautirozen@gmail.com>
2022-08-31 18:56:16 -0300

    added env

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -5,13 +5,13 @@ import "firebase/compat/functions";
 import "firebase/compat/storage";
 
 var firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
+  authDomain: `${process.env.FIREBASE_AUTH_DOMAIN}`,
+  databaseURL: `${process.env.FIREBASE_DATABASE_URL}`,
+  projectId: `${process.env.FIREBASE_PROJECT_ID}`,
+  storageBucket: `${process.env.FIREBASE_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.FIREBASE_MESSAGE_SENDER_ID}`,
+  appId: `${process.env.FIREBASE_APP_ID}`,
 };
 
 if (!firebase.apps.length) {
