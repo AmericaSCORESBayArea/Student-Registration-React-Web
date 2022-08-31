178b103bd5180cc46d9da6fe03887dddbbc4e034
lautirozen <lautirozen@gmail.com>
2022-08-31 18:53:37 -0300

    added english and spanish translations

--- src/components/components/MissingFieldsValidation.js
+++ src/components/components/MissingFieldsValidation.js
@@ -9,83 +9,82 @@ function MissingFieldsValidation(props) {
   const checkBirthdate = isAfterToday(props.values.birthdate);
   const requiredFields = [
     {
-      label: "First Name",
+      label: props.formTranslations.firstName_field,
       value: props.values.firstName,
       position: props.fieldsRef.firstName_field,
     },
     {
-      label: "Last Name",
+      label: props.formTranslations.lastName_field,
       value: props.values.lastName,
       position: props.fieldsRef.lastName_field,
     },
     {
-      label: "Region",
+      label: props.formTranslations.schoolName_region_field,
       value: props.values.schoolName.region,
       position: props.fieldsRef.region_field,
     },
     {
-      label: "School Name",
+      label: props.formTranslations.schoolName_schoolname_field,
       value: props.values.schoolName.schoolname,
       position: props.fieldsRef.schoolName_field,
     },
     {
-      label: "Birthdate",
+      label: props.formTranslations.birthdate_field,
       value: checkBirthdate === true ? props.values.birthdate : "",
       position: props.fieldsRef.birthdate_field,
     },
     {
-      label: "Gender",
+      label: props.formTranslations.gender_field,
       gevaluender: props.values.gender,
       position: props.fieldsRef.gender_field,
     },
     {
-      label: "Grade",
+      label: props.formTranslations.grade_field,
       value: props.values.grade,
       position: props.fieldsRef.grade_field,
     },
     {
-      label: "Ethnicity",
+      label: props.formTranslations.ethnicity_field,
       value: props.values.ethnicity,
       position: props.fieldsRef.ethnicity_field,
     },
     {
-      label: "Does your child receive Free or Reduced Price Lunch?",
+      label: props.formTranslations.reducedPriceLunch_field,
       value: props.values.reducedPriceLunch,
       position: props.fieldsRef.reducedPriceLunch_field,
     },
     {
-      label: "Parent/Guardian First Name",
+      label: props.formTranslations.parentFName_field,
       value: props.values.parentFName,
       position: props.fieldsRef.parentFName_field,
     },
     {
-      label: "Parent/Guardian Last Name",
+      label: props.formTranslations.parentLName_field,
       value: props.values.parentLName,
       position: props.fieldsRef.parentLName_field,
     },
     {
-      label: "Parent/Guardian Phone 1",
+      label: props.formTranslations.parentPhone1_field,
       value: props.values.parentPhone1,
       position: props.fieldsRef.parentPhone_field,
     },
     {
-      label: "Relationship to Child",
+      label: props.formTranslations.relationship_field,
       value: props.values.relationship,
       position: props.fieldsRef.relationship_field,
     },
     {
-      label:
-        "Primary Emergency Contact (Other than Parent/Guardian and Primary Emergency Contact)",
+      label: props.formTranslations.emergency_Contact_Name_field,
       value: props.values.emergency_Contact_Name,
       position: props.fieldsRef.emergency_Contact_Name_field,
     },
     {
-      label: "Emergency Relationship to Child",
+      label: props.formTranslations.emergency_Contact_Relationship_field,
       value: props.values.emergency_Contact_Relationship,
       position: props.fieldsRef.emergency_Contact_Relationship_field,
     },
     {
-      label: "Emergency Phone 1",
+      label: props.formTranslations.emergency_Contact_Phone1_field,
       value: props.values.emergency_Contact_Phone1,
       position: props.fieldsRef.emergency_Contact_Phone1_field,
     },
@@ -101,7 +100,7 @@ function MissingFieldsValidation(props) {
         padding: "15px",
       }}
     >
-      <p>Required Fields Missing :</p>
+      <p>{props.formTranslations.requiredFieldsTitle}</p>
       <ul>
         {requiredFields.map((value) =>
           value.value === "" ? (
