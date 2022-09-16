c3e2988dc3ad59024f182dbd636adb59d00d4d08
lautirozen <lautirozen@gmail.com>
2022-09-15 21:47:53 -0300

    added loading

---
+++ src/components/components/Loading.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+
+export default function Loading(props) {
+  return (
+    <div>
+      <Modal
+        sx={{ backgroundColor: "rgba(0,0,0,0.6)" }}
+        open={props.open}
+        onClose={null}
+      >
+        <Box
+          sx={{
+            alignItems: "center",
+            display: "flex",
+            justifyContent: "center",
+            position: "relative",
+            top: "50%",
+            transform: "translateY(-50%)",
+            outline: "none",
+          }}
+        >
+          <img
+            style={{
+              width: "300px",
+              display: "flex",
+              justifyContent: "center",
+            }}
+            src={require("../../assets/Scores_Logo.gif")}
+            alt=""
+          />
+        </Box>
+      </Modal>
+    </div>
+  );
+}
